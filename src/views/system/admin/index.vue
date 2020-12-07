5d8f0e0cd075bb845e1405fb71dc528900779aa3
Double327 <180303665@qq.com>
2020-12-07 20:39:10 +0800

    模块创建

---
+++ src/views/system/admin/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card>
+    管理员管理
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "Admin"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/system/teacher/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card>
+    教师管理
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "Teacher"
+}
+</script>
+
+<style scoped>
+
+</style>
