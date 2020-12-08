e5c20fd49c2eb8e0d0dbcc9bab7027d02fb8c7dc
Double327 <180303665@qq.com>
2020-12-08 11:15:52 +0800

    组件添加

---
+++ src/views/teach/course/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card>
+    课程管理
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/teach/project/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card>
+    学生项目管理
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
